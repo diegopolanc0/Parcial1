8bafb7ac2011e70c77775290c26a6e2a689f22a2
d1egopolanco <00301417@uca.edu.sv>
2019-06-03 02:29:59 -0600

    :(

--- app/src/main/java/com/example/taller_4_pdm/Activities/Count.kt
+++ app/src/main/java/com/example/taller_4_pdm/Activities/Count.kt
@@ -1,10 +1,11 @@
 package com.example.taller_4_pdm.Activities
 
 import androidx.appcompat.app.AppCompatActivity
-
+import android.view.View;
 
  class Count : AppCompatActivity() {
-
+//TextView showvalue
+  //int counter = 0;
 
 
 }
